1730ea8f29d57fe7be4fad745c3b0d96a22dc9e3
joffreynk <joyjoffrey@gmail.com>
2023-08-12 16:10:05 +0200

    set up onboarding

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,10 +1,15 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
+import AccountProfile from "@/components/form/AccountProfile";
 
 export default function OnBoarding() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <main className="mx-auto flex max-w-3xl flex-col justify-startpx-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threads
+      </p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile />
+      </section>
+    </main>
   );
 }
