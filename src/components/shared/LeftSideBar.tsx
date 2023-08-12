275f324ab11d302edfafd849a69f5687182564e0
joffreynk <joyjoffrey@gmail.com>
2023-08-12 10:56:20 +0200

    add side bar

--- src/components/shared/LeftSideBar.tsx
+++ src/components/shared/LeftSideBar.tsx
@@ -1,8 +1,16 @@
+import { sidebarLinks } from '@/constants'
+import { SideBarProps } from '@/types'
 import React from 'react'
 
 const LeftSideBar = () => {
   return (
-    <div>LeftSideBar</div>
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+        {
+          sidebarLinks.map((link: SideBarProps)=>(<div key={link.route}></div>) )
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type SideBarProps = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
