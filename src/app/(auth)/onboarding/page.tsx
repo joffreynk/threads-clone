00b7296139568b79310afbde519d49369cb0dfa7
joffreynk <joyjoffrey@gmail.com>
2023-08-12 16:34:09 +0200

    add props

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,6 +1,19 @@
 import AccountProfile from "@/components/form/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
 
-export default function OnBoarding() {
+export default async function OnBoarding() {
+  const user  = await currentUser();
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username:userInfo?.username || user?.username,
+    name: userInfo?.name || `${user?.firstName +' '+ user?.lastName}`,
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl
+
+  }
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-startpx-10 py-20">
       <h1 className="head-text">Onboarding</h1>
@@ -8,7 +21,10 @@ export default function OnBoarding() {
         Complete your profile now to use Threads
       </p>
       <section className="mt-9 bg-dark-2 p-10">
-        <AccountProfile />
+        <AccountProfile
+          user={userData}
+          btnTitle='continue'
+         />
       </section>
     </main>
   );
--- src/components/form/AccountProfile.tsx
+++ src/components/form/AccountProfile.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+type AccountProfileProps = {
+  user: {
+    id: string;
+    name: string;
+    objectId: string;
+    image:string;
+    bio:string;
+    username:string
+  },
+  btnTitle: string;
+}
 
-const AccountProfile = () => {
+const AccountProfile = ({user, btnTitle}: AccountProfileProps) => {
   return <div>AccountProfile</div>;
 };
 
