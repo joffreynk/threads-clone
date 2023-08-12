e191aed10647934f39c47a095b987b66943271e5
joffreynk <joyjoffrey@gmail.com>
2023-08-12 12:01:12 +0200

    add right side bar

--- src/components/shared/RightSideBar.tsx
+++ src/components/shared/RightSideBar.tsx
@@ -1,6 +1,10 @@
 
 const RightSideBar = () => {
-  return <div>RightSideBar</div>;
+  return <section className="custom-scrollbar rightsidebar">
+    <div className="flex flex-1 flex-col justify-start">
+      <h3>suggested communities</h3>
+    </div>
+  </section>;
 }
 
 export default RightSideBar
