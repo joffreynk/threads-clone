7c19123d296406d2ee54132b469f9bb253c6a8b6
joffreynk <joyjoffrey@gmail.com>
2023-08-15 16:55:40 +0200

    update mongoose connection

--- lib/mongoConnection.ts
+++ lib/mongoConnection.ts
@@ -1,10 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+let isConnected = false;
 
 export default async function connect() {
-    try {
-      await mongoose.connect(process.env.MONGODB_URL || '');
-      console.log('DB CONNECTED');
-  } catch (error) {
-    console.log('DB CONNECTION ERROR');
+  mongoose.set("strictQuery", true);
+  if (!process.env.MONGODB_URL) {
+    console.log("No connection to Database");
+    throw new Error("No connection to Database");
   }
+  if (isConnected) return console.log('already connected to Database');
+  
+    try {
+      await mongoose.connect(process.env.MONGODB_URL);
+      isConnected = true;
+      console.log("DB CONNECTED");
+    } catch (error) {
+      console.log("DB CONNECTION ERROR");
+    }
 }
