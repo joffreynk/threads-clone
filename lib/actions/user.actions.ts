a35cfa6c2b1a1fdd3de73f84fb2463f8aa3ce3bf
joffreynk <joyjoffrey@gmail.com>
2023-08-15 18:20:55 +0200

    finalize add or edit user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+
+export default async function createAndUpdateUser(
+    userId: string,
+    name: string,
+    username: string,
+    image: string,
+    bio: string,
+    path: string
+) {
+   try {
+      await User.findOneAndUpdate(
+        { _id: userId },
+        {
+          name,
+          username: username.toLocaleLowerCase(),
+          image,
+          bio,
+          onboarded: true,
+        },
+        { upsert: true }
+      );
+      if(path === '/profile/edit') {
+        revalidatePath(path);
+      }
+   } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+   }
+
+    
+}
