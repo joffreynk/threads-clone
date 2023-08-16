2ed374c6b6f2a3391babb91dd001ba81a8cd98b2
joffreynk <joyjoffrey@gmail.com>
2023-08-16 07:02:00 +0200

    write a query to retrieve all threads and children

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { getThreads } from "@/lib/actions/thread.actions"
+
+export default async function Home() {
+
+  const {threads, isNext} = await getThreads(1, 30)
+
   return (
     <main >
       <h1 className="text-white">Threads</h1>
