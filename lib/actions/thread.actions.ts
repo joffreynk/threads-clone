a6dab79ea77469cb3d2cb09dd3b0a187e0c832ed
joffreynk <joyjoffrey@gmail.com>
2023-08-16 06:50:56 +0200

    fetch threads

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -34,3 +34,35 @@ export const createThread = async ({ text, author, communityId, path }: params) 
     throw new Error(`Error creating thread ${error.message}`);
   }
 };
+
+export const getThreads = async(pageNumber = 1, pageSize =20)=>{
+  try {
+    await dbConnection();
+    const skipAmount = (pageNumber-1) * pageSize
+
+    const threads = await Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalThreads = await Thread.countDocuments({parentId: {$in: [null, undefined]}})
+    // const threads = await threadsQuery.exec()
+
+    const isNext = totalThreads > skipAmount + threads.length;
+    
+    return {threads, isNext};
+    
+  } catch (error: any) {
+    console.log(`Error fetching threads ${error.message}`);
+    throw new Error(`Error fetching threads ${error.message}`);
+  }
+}
