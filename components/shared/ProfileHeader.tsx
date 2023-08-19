af117900976dca1831b7e5ac9374fe032685c42f
joffreynk <joyjoffrey@gmail.com>
2023-08-19 21:44:50 +0200

    display image profile

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 type ProfileProps = {
   accountId: string,
   authUserId: string,
@@ -9,8 +11,19 @@ type ProfileProps = {
 
 export default function ProfileHeader({accountId, authUserId, name, username, imageUrl, bio}: ProfileProps) {
   return (
-    <div>
-      
+    <div className="flex w-full flex-col justify-start">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <div className="relative h-20 w-20">
+            <Image
+            src={imageUrl}
+            fill
+            alt={name}
+            className="rounded-full object-cover shadow-2xl"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
