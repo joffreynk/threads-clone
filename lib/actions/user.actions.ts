6f55f1c9380369939353effdda284e167b899157
joffreynk <joyjoffrey@gmail.com>
2023-08-20 07:37:15 +0200

    fetch user with his threads

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import dbConnection from "../mongoConnection";
+import Thread from "../models/thread.model";
 
 type Params = {
    userId: string,
@@ -52,7 +53,6 @@ export  async function getUser(id: String) {
   }
 }
 
-
 export async function getUserById(id: String) {
   try {
     await dbConnection();
@@ -60,5 +60,31 @@ export async function getUserById(id: String) {
   } catch (error: any) {
     console.log(`Failed to create/update user: ${error.message}`);
     return new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
+
+export async function getUsersPosts(id: string){
+  try {
+    await dbConnection();
+
+    // TODO: populate communities on this user
+    const threads = await User.findOne({id})
+      .populate({
+        path: 'threads',
+        model: Thread,
+        populate: {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'name image id',
+          }
+        }
+      })
+
+      return threads;
+  } catch (error: any) {
+    throw new Error(`Error fetching threads: ${error.message}`);
   }
 }
