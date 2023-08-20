156150912f140687853056cff9a64e2ee687cfdc
joffreynk <joyjoffrey@gmail.com>
2023-08-20 13:12:44 +0200

    add activity profiler

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { getMyActivities, getUser, searchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+  const userInfo = await getUser(user.id);
+  if (!userInfo) return redirect("/onboarding");
+
+  const actitvies = await getMyActivities(userInfo._id);
   return (
     <section>
-      <h1 className='head-text mb-10'>Activity</h1>
+      <h1 className="head-text mb-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {actitvies.length < 0 ? (
+          <p className="no-result">No activity yet</p>
+        ): (
+          actitvies.map((activity) => (
+            <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+              <article className="activity-card">
+                <Image
+                src={activity.author.image}
+                alt="profile photo"
+                width={30}
+                height={30}
+                className="rounded-full object-cover"
+                />
+              </article>
+            </Link>
+          ))
+        )}
+      </section>
     </section>
-  )
+  );
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
@@ -7,13 +7,13 @@ import Thread from "../models/thread.model";
 import { FilterQuery, SortOrder } from "mongoose";
 
 type Params = {
-   userId: string,
-    name: string,
-    username: string,
-    image: string,
-    bio: string,
-    path: string
-}
+  userId: string;
+  name: string;
+  username: string;
+  image: string;
+  bio: string;
+  path: string;
+};
 
 export async function createAndUpdateUser({
   userId,
@@ -44,10 +44,10 @@ export async function createAndUpdateUser({
   }
 }
 
-export  async function getUser(id: String) {
+export async function getUser(id: String) {
   try {
     await dbConnection();
-    return await User.findOne({id})
+    return await User.findOne({ id });
   } catch (error: any) {
     console.log(`Failed to create/update user: ${error.message}`);
     return new Error(`Failed to create/update user: ${error.message}`);
@@ -57,34 +57,33 @@ export  async function getUser(id: String) {
 export async function getUserById(id: String) {
   try {
     await dbConnection();
-    return await User.findById(id );
+    return await User.findById(id);
   } catch (error: any) {
     console.log(`Failed to create/update user: ${error.message}`);
     return new Error(`Failed to create/update user: ${error.message}`);
   }
 }
 
-export async function getUsersThreads(id: string){
+export async function getUsersThreads(id: string) {
   try {
     await dbConnection();
 
     // TODO: populate communities on this user
-    const threads = await User.findOne({id})
-      .populate({
-        path: 'threads',
+    const threads = await User.findOne({ id }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
         model: Thread,
         populate: {
-          path: 'children',
-          model: Thread,
-          populate: {
-            path: 'author',
-            model: User,
-            select: 'name image id',
-          }
-        }
-      })
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
 
-      return threads;
+    return threads;
   } catch (error: any) {
     throw new Error(`Error fetching threads: ${error.message}`);
   }
@@ -105,18 +104,18 @@ export async function searchUsers({
 }) {
   try {
     await dbConnection();
-    const skipAmount = (pageNumber-1)*pageSize;
-    const regex = new RegExp(searchString, 'i');
-    const query: FilterQuery<typeof User> = {id: {$ne: userId}};
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+    const query: FilterQuery<typeof User> = { id: { $ne: userId } };
 
-    if(searchString.trim() !== ''){
+    if (searchString.trim() !== "") {
       query.$or = [
         { name: { $regex: regex } },
         { username: { $regex: regex } },
       ];
     }
 
-    const sortOptions = {createdAt: sortBy}
+    const sortOptions = { createdAt: sortBy };
 
     const users = await User.find(query)
       .skip(skipAmount)
@@ -126,11 +125,32 @@ export async function searchUsers({
 
     const totalUsers = await User.countDocuments(query);
 
-    const isNext = totalUsers > users.length
+    const isNext = totalUsers > users.length;
 
-    return {users, isNext}
-
+    return { users, isNext };
   } catch (error: any) {
-    throw new Error(`Error fetching users: ${error.message}`)
+    throw new Error(`Error fetching users: ${error.message}`);
+  }
+}
+
+export async function getMyActivities(author: string) {
+  try {
+    await dbConnection();
+    const myThreads = await Thread.find({ author });
+
+    const idChildrenThreads: [] = myThreads.reduce((acc, thread) =>acc.concat(thread.children), []);
+
+    const replies = await Thread.find({
+      _id: { $in: idChildrenThreads },
+      author: { $ne: author },
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id'
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Error fetching my activities: ${error.message}`);
   }
 }
